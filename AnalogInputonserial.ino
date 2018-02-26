72f5dd509ce4cf49f058acf6baf10d3d4cadb1e8
Sagar salunkhe <sagsal92@gmail.com>
2018-02-26 21:46:41 +0530

    Add files via upload

---
+++ AnalogInputonserial.ino
@@ -0,0 +1,15 @@
+int sensorPin = A0;
+int sensorValue = 0;
+
+void setup() 
+{
+  pinMode(sensorPin, IN PUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(100);
+}
